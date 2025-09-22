005cbf5ea2e4420f3101ec5f2c3a969d4a2850b7
Shubham Deshmukh <shubhamdeshmukh69@gmail.com>
2025-09-22 13:03:43 +0530

    Initial Commit

---
+++ SegmentedPickerPOC/SegmentedPickerPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  SegmentedPickerPOCApp.swift
+//  SegmentedPickerPOC
+//
+//  Created by Shubham Deshmukh on 22/09/25.
+//
+
+import SwiftUI
+
+@main
+struct SegmentedPickerPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
